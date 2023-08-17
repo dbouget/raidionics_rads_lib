c6bff9687edacd448fd716e026d7158d07a7b3bc
dbouget <bougetd@gmail.com>
2023-08-17 14:51:00 +0200

    Updating windows CI, removing medpy

--- setup.py
+++ setup.py
@@ -9,15 +9,10 @@ with open('requirements.txt', 'r', encoding='utf-16', errors='ignore') as ff:
     required = ff.read().splitlines()
 
 if platform.system() == 'Windows':
-    required.append('antspy@https://github.com/SGotla/ANTsPy/releases/download/0.1.7Win64/antspy-0.1.7-cp37-cp37m-win_amd64.whl')
-    # required.append('pandas==1.3.5')
-    required.append('scikit-learn==1.0.2')
-    required.append('statsmodels==0.13.2')
+    required.append('antspyx@https://github.com/ANTsX/ANTsPy/releases/download/v0.3.8/antspyx-0.3.8-cp38-cp38-win_amd64.whl')
 elif platform.system() == 'Darwin' and platform.processor() == 'arm':   # Specific for Apple M1 chips
-    required.append('pandas==1.3.5')
-    required.append('scikit-learn==1.0.2')
-    required.append('statsmodels==0.13.2')
-    required.append('MedPy==0.4.0')
+    required.append('scikit-learn==1.3.0')
+    required.append('statsmodels==0.14.0')
 elif platform.system() == 'Darwin':
     required.append('antspyx@https://github.com/ANTsX/ANTsPy/releases/download/v0.3.8/antspyx-0.3.8-cp38-cp38-macosx_10_9_x86_64.whl')
 else:
