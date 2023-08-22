58013a5459d4438577080254243c95420c14d937
dbouget <bougetd@gmail.com>
2023-08-22 15:57:13 +0200

    Reverting ANTs version on Windows [skip ci]

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ with open('requirements.txt', 'r', encoding='utf-16', errors='ignore') as ff:
     required = ff.read().splitlines()
 
 if platform.system() == 'Windows':
-    required.append('antspyx@https://github.com/ANTsX/ANTsPy/releases/download/v0.3.8/antspyx-0.3.8-cp37-cp37m-win_amd64.whl')
+    # required.append('antspyx@https://github.com/ANTsX/ANTsPy/releases/download/v0.3.8/antspyx-0.3.8-cp37-cp37m-win_amd64.whl')
+    required.append('antspy@https://github.com/SGotla/ANTsPy/releases/download/0.1.7Win64/antspy-0.1.7-cp37-cp37m-win_amd64.whl')
 #     # required.append('pandas==1.3.5')
-#     required.append('scikit-learn==1.0.2')
-#     required.append('statsmodels==0.13.2')
-if platform.system() == 'Darwin' and platform.processor() == 'arm':   # Specific for Apple M1 chips
+    required.append('scikit-learn==1.0.2')
+    required.append('statsmodels==0.13.2')
+elif platform.system() == 'Darwin' and platform.processor() == 'arm':   # Specific for Apple M1 chips
     # required.append('pandas==1.3.5')
     required.append('scikit-learn==1.0.2')
     required.append('statsmodels==0.13.2')
