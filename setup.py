3a18e97ec0221153c754366b997776ec366b652c
David Bouget <dbouget@users.noreply.github.com>
2024-02-13 16:23:46 +0100

    Ci update (#5)
    * Updating CI actions versioning, pyANTSx version, Python build version
    
    * Setting the macOS ARM CI to run on Github
    
    * Manifest must include ANTs for the macOS ARM to bundle properly
    
    * Typo in registration unit test for macOS ARM [skip ci]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.system() == 'Darwin' and platform.processor() == 'arm':   # Specific
     required.append('scikit-learn')
     required.append('statsmodels')
 else:
-    required.append('antspyx==0.3.8')
+    required.append('antspyx==0.4.2')
 
 setup(
     name='raidionicsrads',
@@ -37,8 +37,8 @@ setup(
     },
     install_requires=required,
     include_package_data=True,
-    python_requires=">=3.7",
-    version='1.1.1',
+    python_requires=">=3.8",
+    version='1.1.2',
     author='David Bouget (david.bouget@sintef.no)',
     license='BSD 2-Clause',
     description='Raidionics reporting and data system backend (RADS)',
