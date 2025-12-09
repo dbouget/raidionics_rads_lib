918c59584107a8385dac9a201a846d3b3c003394
dbouget <david.bouget@sintef.no>
2025-12-09 08:07:56 +0000

    Fixing docker test and deprecating macos-13 [skip ci]

--- tests/local_docker_tests/test_registration_pipeline_docker.py
+++ tests/local_docker_tests/test_registration_pipeline_docker.py
@@ -26,16 +26,21 @@ def test_registration_pipeline_docker(test_dir, tmp_path):
         if os.environ.get("GITHUB_ACTIONS"):
             image_name = "dbouget/raidionics-rads:" + os.environ["IMAGE_TAG"]
 
-        output_folder = os.path.join(tmp_path, "output_reg_docker")
+        output_folder = os.path.join(tmp_path, "results")
         if os.path.exists(output_folder):
             shutil.rmtree(output_folder)
         os.makedirs(output_folder)
-        #
-        # test_raw_input_fn = os.path.join(test_dir, "patients", 'patient-UnitTest1')
-        # tmp_test_input_fn = os.path.join(tmp_path, "results", "input_reg_docker")
-        # if os.path.exists(tmp_test_input_fn):
-        #     shutil.rmtree(tmp_test_input_fn)
-        # shutil.copytree(test_raw_input_fn, tmp_test_input_fn)
+
+        test_raw_input_fn = os.path.join(test_dir, "patients")
+        tmp_test_input_fn = os.path.join(tmp_path, "patients")
+        if os.path.exists(tmp_test_input_fn):
+            shutil.rmtree(tmp_test_input_fn)
+        shutil.copytree(test_raw_input_fn, tmp_test_input_fn)
+        test_raw_models_fn = os.path.join(test_dir, "models")
+        tmp_test_model_fn = os.path.join(tmp_path, "models")
+        if os.path.exists(tmp_test_model_fn):
+            shutil.rmtree(tmp_test_model_fn)
+        shutil.copytree(test_raw_models_fn, tmp_test_model_fn)
 
         using_skull_stripped_inputs = False
         logging.info("Preparing configuration file.\n")
@@ -54,7 +59,7 @@ def test_registration_pipeline_docker(test_dir, tmp_path):
             rads_config.set('Runtime', 'reconstruction_method', 'thresholding')
             rads_config.set('Runtime', 'reconstruction_order', 'resample_first')
             rads_config.set('Runtime', 'use_stripped_data', 'True' if using_skull_stripped_inputs else 'False')
-            rads_config_filename = os.path.join(test_dir, "results", 'rads_config.ini')
+            rads_config_filename = os.path.join(tmp_path, "results", 'rads_config.ini')
             with open(rads_config_filename, 'w') as outfile:
                 rads_config.write(outfile)
 
@@ -102,13 +107,13 @@ def test_registration_pipeline_docker(test_dir, tmp_path):
             pipeline_json[step_str]["direction"] = "forward"
             pipeline_json[step_str]["description"] = "Apply registration from T1CE (T0) to T1CE (T0)."
 
-            with open(os.path.join(test_dir, "results", 'test_pipeline.json'), 'w', newline='\n') as outfile:
+            with open(os.path.join(tmp_path, "results", 'test_pipeline.json'), 'w', newline='\n') as outfile:
                 json.dump(pipeline_json, outfile, indent=4, sort_keys=True)
 
             logging.info("Running registration pipeline unit test in Docker container.\n")
             try:
                 import platform
-                cmd_docker = ['docker', 'run', '-v', '{}:/workspace/resources'.format(test_dir),
+                cmd_docker = ['docker', 'run', '-v', '{}:/workspace/resources'.format(tmp_path),
                               '--network=host', '--ipc=host']
                 if not os.environ.get("GITHUB_ACTIONS") and sys.platform != "win32":
                     cmd_docker.extend(['--user', str(os.geteuid())])
@@ -122,26 +127,26 @@ def test_registration_pipeline_docker(test_dir, tmp_path):
                     subprocess.check_call(cmd_docker, stdout=sys.stdout, stderr=sys.stderr)
             except Exception as e:
                 logging.error(f"Error during registration pipeline unit test in Docker container with: {e}\n {traceback.format_exc()}.\n")
-                if os.path.exists(test_dir):
-                    shutil.rmtree(test_dir)
+                if os.path.exists(tmp_path):
+                    shutil.rmtree(tmp_path)
                 raise ValueError("Error during registration pipeline unit test in Docker container.\n")
 
             logging.info("Collecting and comparing results.\n")
-            transform_dir = os.listdir(os.path.join(test_dir, "results", "Transforms"))
+            transform_dir = os.listdir(os.path.join(tmp_path, "results", "Transforms"))
             assert len(transform_dir) > 0, "No transform folder was generated"
-            registered_inputs = os.listdir(os.path.join(test_dir, "results", "T0", "T0_T1c_space"))
+            registered_inputs = os.listdir(os.path.join(tmp_path, "results", "T0", "T0_T1c_space"))
             assert len(registered_inputs) > 0, "No registered files were generated"
 
             logging.info("Registration unit test in Docker container succeeded.\n")
         except Exception as e:
             logging.error(f"Error during registration pipeline unit test in Docker container with: {e}\n {traceback.format_exc()}.\n")
-            if os.path.exists(test_dir):
-                shutil.rmtree(test_dir)
+            if os.path.exists(tmp_path):
+                shutil.rmtree(tmp_path)
             raise ValueError("Error during registration pipeline unit test in Docker container with.\n")
     except Exception as e:
         logging.error(f"Error during registration pipeline unit test in Docker container with: {e}\n {traceback.format_exc()}.\n")
         raise ValueError("Error during registration pipeline unit test in Docker container.\n")
 
     logging.info("Registration pipeline unit test in Docker container succeeded.\n")
-    if os.path.exists(test_dir):
-        shutil.rmtree(test_dir)
+    if os.path.exists(tmp_path):
+        shutil.rmtree(tmp_path)
--- tests/local_docker_tests/test_segmentation_pipeline_docker.py
+++ tests/local_docker_tests/test_segmentation_pipeline_docker.py
@@ -10,7 +10,7 @@ import nibabel as nib
 import numpy as np
 
 
-def test_segmentation_pipeline_docker(test_dir):
+def test_segmentation_pipeline_docker(test_dir, tmp_path):
     """
     Testing the CLI within a Docker container for the segmentation pipeline unit test, running on CPU.
     The latest Docker image is being hosted at: dbouget/raidionics-rads:v1.3.1-py39-cpu
@@ -27,6 +27,24 @@ def test_segmentation_pipeline_docker(test_dir):
         image_name = "dbouget/raidionics-rads:v1.3.1-py39-cpu"
         if os.environ.get("GITHUB_ACTIONS"):
             image_name = "dbouget/raidionics-rads:" + os.environ["IMAGE_TAG"]
+
+        output_folder = os.path.join(tmp_path, "results")
+        if os.path.exists(output_folder):
+            shutil.rmtree(output_folder)
+        os.makedirs(output_folder)
+
+        test_raw_input_fn = os.path.join(test_dir, "patients")
+        tmp_test_input_fn = os.path.join(tmp_path, "patients")
+        if os.path.exists(tmp_test_input_fn):
+            shutil.rmtree(tmp_test_input_fn)
+        shutil.copytree(test_raw_input_fn, tmp_test_input_fn)
+        test_raw_models_fn = os.path.join(test_dir, "models")
+        tmp_test_model_fn = os.path.join(tmp_path, "models")
+        if os.path.exists(tmp_test_model_fn):
+            shutil.rmtree(tmp_test_model_fn)
+        shutil.copytree(test_raw_models_fn, tmp_test_model_fn)
+
+        using_skull_stripped_inputs = False
         rads_config = configparser.ConfigParser()
         rads_config.add_section('Default')
         rads_config.set('Default', 'task', 'neuro_diagnosis')
@@ -40,7 +58,8 @@ def test_segmentation_pipeline_docker(test_dir):
         rads_config.add_section('Runtime')
         rads_config.set('Runtime', 'reconstruction_method', 'thresholding')
         rads_config.set('Runtime', 'reconstruction_order', 'resample_first')
-        rads_config_filename = os.path.join(test_dir, "results", 'rads_config.ini')
+        rads_config.set('Runtime', 'use_stripped_data', 'True' if using_skull_stripped_inputs else 'False')
+        rads_config_filename = os.path.join(tmp_path, "results", 'rads_config.ini')
         with open(rads_config_filename, 'w') as outfile:
             rads_config.write(outfile)
 
@@ -63,13 +82,13 @@ def test_segmentation_pipeline_docker(test_dir):
         pipeline_json[step_str]["format"] = "thresholding"
         pipeline_json[step_str]["description"] = "Identifying the best brain segmentation model for existing inputs"
 
-        with open(os.path.join(test_dir, "results", 'test_pipeline.json'), 'w', newline='\n') as outfile:
+        with open(os.path.join(tmp_path, "results", 'test_pipeline.json'), 'w', newline='\n') as outfile:
             json.dump(pipeline_json, outfile, indent=4, sort_keys=True)
 
         logging.info("Running segmentation pipeline unit test in Docker container.\n")
         try:
             import platform
-            cmd_docker = ['docker', 'run', '-v', '{}:/workspace/resources'.format(test_dir),
+            cmd_docker = ['docker', 'run', '-v', '{}:/workspace/resources'.format(tmp_path),
                           '--network=host', '--ipc=host']
             if not os.environ.get("GITHUB_ACTIONS") and sys.platform != "win32":
                 cmd_docker.extend(['--user', str(os.geteuid())])
@@ -85,10 +104,10 @@ def test_segmentation_pipeline_docker(test_dir):
             raise ValueError(f"{e}")
 
         logging.info("Collecting and comparing results.\n")
-        segmentation_pred_filename = os.path.join(test_dir, "results", 'T0',
+        segmentation_pred_filename = os.path.join(tmp_path, "results", 'T0',
                                                   'input1_annotation-Brain_MRI_Brain.nii.gz')
         assert os.path.exists(segmentation_pred_filename), "No brain segmentation mask was generated.\n"
-        segmentation_gt_filename = os.path.join(test_dir, "patients", "patient-UnitTest1", "verif",
+        segmentation_gt_filename = os.path.join(tmp_path, "patients", "patient-UnitTest1", "verif",
                                                 'T0', 'input1_annotation-Brain.nii.gz')
         segmentation_pred = nib.load(segmentation_pred_filename).get_fdata()[:]
         segmentation_gt = nib.load(segmentation_gt_filename).get_fdata()[:]
@@ -96,6 +115,10 @@ def test_segmentation_pipeline_docker(test_dir):
                               segmentation_gt), "Ground truth and prediction arrays are not identical"
     except Exception as e:
         logging.error(f"Error during segmentation pipeline unit test in Docker container with: {e}\n {traceback.format_exc()}.\n")
+        if os.path.exists(tmp_path):
+            shutil.rmtree(tmp_path)
         raise ValueError("Error during segmentation pipeline unit test in Docker container.\n")
 
     logging.info("Segmentation pipeline unit test in Docker container succeeded.\n")
+    if os.path.exists(tmp_path):
+        shutil.rmtree(tmp_path)
