d8f03cfb1a1847cfbea9faa0d6b1d004a3dc86ad
dbouget <bougetd@gmail.com>
2026-04-14 14:41:11 +0200

    Updating CIs [skip ci]

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         ],
     },
     python_requires=">=3.9",
-    version='1.3.2',
+    version='1.3.3',
     author='David Bouget (david.bouget@sintef.no)',
     license='BSD 2-Clause',
     description='Raidionics reporting and data system backend (RADS)',
